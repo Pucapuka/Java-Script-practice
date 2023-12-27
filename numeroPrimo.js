5b5da1148626a78c2688dbe17c783cb43816787a
Pucapuka <traderstrap1@gmail.com>
2023-12-27 17:11:54 -0300

    praticando JS: algoritmo para verificar se o número armazenado na variável é primo

---
+++ numeroPrimo.js
@@ -0,0 +1,12 @@
+let numero = 53;
+
+    for(let i=2; i<numero-1; i++){
+        if (numero%i===0){
+            console.log(i);
+            console.log("Numero não é primo.");
+            return;
+        }else{
+            continue;
+        }
+    }
+    console.log("número primo");
