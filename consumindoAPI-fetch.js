0536cf8d84cbdde8b935e12a50855a086f1f5d97
Paulo Lima <102117492+Pucapuka@users.noreply.github.com>
2024-12-25 15:17:20 -0300

    Update consumindoAPI-fetch.js
    made it in a more versatile way, using images.

--- consumindoAPI-fetch.js
+++ consumindoAPI-fetch.js
@@ -1,30 +1,31 @@
-const url = "https://jsonplaceholder.typicode.com/posts";
-const dados= document.getElementById("script-data");
+const dataField = document.getElementById('data-field');
+const url = "https://api.sampleapis.com/coffee/hot";
 
 fetch(url)
-    .then(response => {
-        if(!response.ok){
-            throw new Error("Erro ao buscar dados");
-        }
-        return response.json();
-    })
-    .then(data =>{
-        console.log(data);
-        
-        let content = '';
-        
-        for(let index in data){
-            content += `
-                <div>
-                    <h3>Id: ${data[index].id}<h3>
-                    <h3>Título: ${data[index].title}</h3>
-                    <p>Corpo: ${data[index].body}</p>
-                </div>
-                <hr>
-            `
-        }
-            dados.innerHTML = content;
-    })
-    .catch(e => {
-        console.error('Erro: ', e);
-    })
+.then(response => {
+
+    if(!response.ok){
+        throw new Error(`Error fetching data. Status: ${response.status}`);
+    }
+    return response.json();
+    
+})
+.then(data => {
+    
+    console.log(data);
+    let content = '';
+    
+    for(let index in data){
+        content += `
+            <h3>Title: ${data[index].title}</h3>
+            <p>Description: ${data[index].description}<p>
+            <p>Ingredients: ${data[index].ingredients.join(',')}</p>
+            <img src='${data[index].image}' alt='${data[index].title}' style="width:500px; height: 500px;">
+        `;
+    }
+    
+    dataField.innerHTML = content;
+})
+.catch(error => {
+    console.error(`Error: ${error}`);
+})
