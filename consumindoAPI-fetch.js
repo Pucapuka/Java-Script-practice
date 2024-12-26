9ca6112278c1aea4fe5235da58acc592518fb489
Paulo Lima <102117492+Pucapuka@users.noreply.github.com>
2024-12-26 12:43:07 -0300

    Update consumindoAPI-fetch.js
    Implemented another way of consuming the API, manipulating more the DOM-tree.

--- consumindoAPI-fetch.js
+++ consumindoAPI-fetch.js
@@ -29,3 +29,49 @@ fetch(url)
 .catch(error => {
     console.error(`Error: ${error}`);
 })
+
+
+/*
+Another way that can be done, manipulating DOM, is the one below:
+
+const url = 'https://api.sampleapis.com/coffee/hot';
+let container = document.getElementById('container');
+fetch(url)
+    .then(function(response){
+        if(!response.ok){
+            throw new Error("Fetch error!");
+        }
+        return response.json();
+    }).then(function(data){
+        console.log(data);
+        
+        data.forEach(coffee => {
+            const title = document.createElement('h3');
+            title.classList.add('text-content');
+            title.textContent = coffee.title;
+            
+            const description = document.createElement('p');
+            description.classList.add('text-content');
+            description.textContent = coffee.description;
+            
+            const ingredients = document.createElement('p');
+            ingredients.classList.add('text-content');
+            ingredients.textContent = `Igredients: ${coffee.ingredients}`;
+            
+            const image = document.createElement('img');
+            image.classList.add('image');
+            image.src = coffee.image;
+            image.alt = coffee.title;
+            
+            container.appendChild(title);
+            container.appendChild(description);
+            container.appendChild(ingredients);
+            container.appendChild(image);
+        });
+    }).catch(function(error){
+        console.error(`Error: ${error.message}`);
+        container.innerHTML = `Error: No content available.`;
+    });
+    
+    
+*/
