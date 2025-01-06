c8dc804c61516b85e072d13988b2e3bb84cc4ef9
Paulo Lima <102117492+Pucapuka@users.noreply.github.com>
2025-01-06 10:55:47 -0300

    Delete contador-de-palavras directory
    apagando diretório para colocá-lo em um repositório separado (para facilitar o deploy).

--- contador-de-palavras/script.js
+++
@@ -1,8 +0,0 @@
-function counter(){
-    const text = document.querySelector('.text-area').value;
-    const numberOfWords = document.querySelector("#number-of-words");
-    const words = text.match(/[áàãâäéèêëíìîïóòõôöúùûüçÁÀÃÂÄÉÈÊËÍÌÎÏÓÒÕÔÖÚÙÛÜÇa-zA-Z0-9]+/g);
-    const wordCount = words? words.length : 0;
-    
-    numberOfWords.innerHTML = `Número de palavras: ${wordCount}`;
-}
